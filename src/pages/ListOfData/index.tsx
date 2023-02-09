ee4acd6936910a2cdffbcf72fd30f929aa9cb502
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-09 13:00:32 +0600

    data table added

--- src/pages/ListOfData/index.tsx
+++ src/pages/ListOfData/index.tsx
@@ -1,9 +1,28 @@
 import { Box } from "@mui/material";
 import usePosts from "../../hooks/usePosts";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "userId", headerName: "User ID", width: 70 },
+  { field: "title", headerName: "Title", width: 400 },
+  { field: "body", headerName: "Body", width: 600 },
+];
 
 const ListOfData = () => {
   const { data, isLoading } = usePosts();
 
-  return <Box></Box>;
+  return (
+    <Box sx={{ height: 680, width: "100%", px: [2, 3], py: [2, 3] }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={10}
+        // rowsPerPageOptions={[5]}
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Box>
+  );
 };
 export default ListOfData;
