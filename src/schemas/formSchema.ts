8ed0560b82ffa067e39b8648f9bb3577217a8e14
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-09 12:16:19 +0600

    formSchema created

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,1 +1,11 @@
 import * as yup from "yup";
+
+const required = "required";
+
+const formSchema = yup.object().shape({
+  fullName: yup.string().required(required),
+  phoneNumber: yup.string().min(11).max(14).required(required),
+  email: yup.string().email("enter an valid email").required(required),
+});
+
+export default formSchema;
